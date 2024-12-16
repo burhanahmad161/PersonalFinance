c2cce953eca600875a39a57894ae41593023a425
burhanahmad161 <2022cs191@uet.edu.pk>
2024-12-16 02:45:51 -0800

    Setup Next Project

---
+++ constants.js
@@ -0,0 +1,30 @@
+export const navOptions = [
+    {
+      title: "OverView",
+      link: "/",
+    },
+    {
+      title: "Wallet",
+      link: "/AddExpense",
+    },
+    {
+      title: "Analytics",
+      link: "/TrackExpense",
+    },
+    {
+      title: "Transactions",
+      link: "/TransferExpense",
+    },
+    {
+      title: "Help",
+      link: "/TransferExpense",
+    },
+    {
+      title: "Settings",
+      link: "/TransferExpense",
+    },{
+      title: "Report",
+      link: "/TransferExpense",
+    },
+  ];
+  
